29a1ca0f30e3e30df5903ea1e933122f483a13de
Vaibhav <vaibhavtatkare04@gmail.com>
2024-11-23 03:30:48 +0530

    variables usage in java

---
+++ variable.java
@@ -0,0 +1,15 @@
+class variable {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 10;
+        int sum = x + y;
+        System.out.println("Addition of " + x + " and " + y + " is " + sum);
+
+        int v1 = x/y;
+        int v2 = y/x;
+        int v3 = x%y;
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+    }
+}
