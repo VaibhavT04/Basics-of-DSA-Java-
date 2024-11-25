084aaac63f90a0cd3a022c10054f48fe74b7ad36
Vaibhav <vaibhavtatkare04@gmail.com>
2024-11-26 00:49:20 +0530

    input/outputs and optimized prime number

---
+++ Java Basics/ipnop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ipnop {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int age = Integer.parseInt(scn.nextLine());
+        String name = scn.nextLine();
+
+        System.out.println("Hello " + name + ". Your age is " + age);
+    }
+}
---
+++ Java Basics/prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+
+        for(int i=0; i<t; i++){
+            int n = scn.nextInt();
+            int count = 0;
+
+            for(int d=2; d*d<=n; d++){
+                if(n%d == 0){
+                    count++;
+                }
+            }
+
+            if(count > 0)
+                { System.out.println("not prime"); }
+            else
+                { System.out.println("prime"); }
+        }
+
+        scn.close();
+    }
+}
