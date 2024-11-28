19a067c1055f99b27047466ed7ff4afa9f4dc60d
Vaibhav <vaibhavtatkare04@gmail.com>
2024-11-29 02:15:03 +0530

    print digits of number in reverse

---
+++ Java Basics/reverse_print.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverse_print {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        while(n>0){
+            int x = n%10;
+            System.out.println(x);
+            n = n/10;
+        }
+
+        // System.out.println(length);
+        scn.close();
+    }
+}
